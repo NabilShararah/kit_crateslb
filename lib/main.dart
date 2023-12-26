3fec84d77d7e240920e33c1a8ddc33f97ecabd1a
Nabil <nabilshararah8@gmail.com>
2023-12-26 15:45:53 +0200

    update1

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.amber,
+          title: Text('Kit Crates ',
+            style: TextStyle(
+                fontSize:28,
+                color :Colors.black),),
+
+        ),
+
+
+        body: Center(
+          child: Home(),
+
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
